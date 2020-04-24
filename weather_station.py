71ac9d5b6301c0f052926787035463cd9d8384d3
Asgeir Vinkenes <asgeirv@protonmail.com>
2020-04-24 19:10:07 +0200

    Show IP

--- weather_station.py
+++ weather_station.py
@@ -8,6 +8,7 @@ from datetime import datetime
 import traceback
 import epd4in2
 import yr
+import socket 
 
 font_smallest = ImageFont.truetype("/usr/share/fonts/truetype/lato/Lato-Regular.ttf", 12)
 font_small = ImageFont.truetype("/usr/share/fonts/truetype/lato/Lato-Regular.ttf", 16)
@@ -15,72 +16,72 @@ font_med = ImageFont.truetype("/usr/share/fonts/truetype/lato/Lato-Regular.ttf",
 font_big = ImageFont.truetype("/usr/share/fonts/truetype/lato/Lato-Regular.ttf", 32)
 font_biggest = ImageFont.truetype("/usr/share/fonts/truetype/lato/Lato-Regular.ttf", 48)
 
-def print_weather():	
+def print_weather():
 	try:
 		data = yr.get_forecast()
 		draw_weather(data)
-		
+
 	except:
-		draw_error("Sjekk last_error.txt")
-		
+		draw_error("Sjekk last_error.txt | IP: " + get_host_ip())
+
 def draw_weather(data):
 	# Get current weather
 	weather_now = data[0]
-	
+
 	icon_now = weather_now["icon"]["var"]
 	icon_name_now = weather_now["icon"]["text"]
-	
+
 	temperature_val_now = weather_now["temperature"]["val"]
 	temperature_unit_now = weather_now["temperature"]["unit"]
-	
+
 	wind_speed_now = weather_now["wind_speed"]["speed"]
 	wind_name_now = weather_now["wind_speed"]["name"]
 	wind_direction_now = weather_now["wind_direction"]["direction"]
-	
+
 	pressure_val_now = weather_now["pressure"]["val"]
 	pressure_unit_now = weather_now["pressure"]["unit"]
-	
+
 	# Get future weather 1
 	weather1 = data[1]
-	
+
 	time_1 = weather1["time"]
-	
+
 	icon_1 = weather1["icon"]["var"]
 	icon_name_1 = weather1["icon"]["text"]
-	
+
 	temperature_val_1 = weather1["temperature"]["val"]
 	temperature_unit_1 = weather1["temperature"]["unit"]
-	
+
 	wind_speed_1 = weather1["wind_speed"]["speed"]
-	
+
 	# Get future weather 2
 	weather2 = data[2]
-	
+
 	time_2 = weather2["time"]
-	
+
 	icon_2 = weather2["icon"]["var"]
 	icon_name_2 = weather2["icon"]["text"]
-	
+
 	temperature_val_2 = weather2["temperature"]["val"]
 	temperature_unit_2 = weather2["temperature"]["unit"]
-	
+
 	wind_speed_2 = weather2["wind_speed"]["speed"]
-	
+
 	# Get credits
 	credits = yr.get_credits()
-	
+
 	# Initialize E-Ink screen
 	epd = epd4in2.EPD()
 	epd.init()
 	epd.Clear(0xFF)
-		
+
 	print("Drawing weather report...")
-		
+
 	#Initialize image
 	image = Image.new("1", (epd4in2.EPD_WIDTH, epd4in2.EPD_HEIGHT), 255)
-		
+
 	draw = ImageDraw.Draw(image)
-		
+
 	# Draw current weather
 	# Weather icon
 	icon = Image.open("icons/%s.bmp" % (icon_now))
@@ -95,10 +96,10 @@ def draw_weather(data):
 	draw.text((300, 50), "%s" % (wind_direction_now), font = font_small, fill = 0)
 	# Pressure
 	draw.text((300, 70), "%s %s" % (pressure_val_now, pressure_unit_now), font = font_small, fill = 0)
-	
+
 	# Draw separator
 	draw.line((10, 150, 390, 150), fill = 0)
-	
+
 	# Draw future weather 1
 	# Time
 	draw.text((20, 160), time_1, font = font_small, fill = 0)
@@ -109,7 +110,7 @@ def draw_weather(data):
 	draw.text((80, 175), u"%s° %s" % (temperature_val_1, temperature_unit_1), font = font_med, fill = 0)
 	# Wind
 	draw.text((80, 200), "%s" % (wind_speed_1), font = font_small, fill = 0)
-	
+
 	# Draw future weather 2
 	# Time
 	draw.text((200, 160), time_2, font = font_small, fill = 0)
@@ -120,40 +121,47 @@ def draw_weather(data):
 	draw.text((260, 175), u"%s° %s" % (temperature_val_2, temperature_unit_2), font = font_med, fill = 0)
 	# Wind
 	draw.text((260, 200), "%s" % (wind_speed_2), font = font_small, fill = 0)
-	
+
 	# Draw separator
 	draw.line((10, 235, 390, 235), fill = 0)
-		
+
 	# Credits
 	draw.text((15, 240), credits[0], font = font_small, fill = 0)
 	draw.text((15, 260), credits[1], font = font_small, fill = 0)
-	# Last updated
-	draw.text((15, 280), "Sist oppdatert %s" % (datetime.now()), font = font_smallest, fill = 0)
-		
+	# Last updated + IP
+	draw.text((15, 280), "Sist oppdatert %s | IP: %s" % (datetime.now(), get_host_ip()), font = font_smallest, fill = 0)
+
 	# Draw weather report
 	epd.display(epd.getbuffer(image))
 	time.sleep(2)
 	epd.sleep()
-		
+
 def draw_error(err_msg):
 	with open("last_error.txt", "w") as err_file:
 		err_file.write('traceback.format_exc():\n' + traceback.format_exc())
-			
+
 	epd = epd4in2.EPD()
 	epd.init()
 	epd.Clear(0xFF)
-		
+
 	#Initialize image
 	image = Image.new("1", (epd4in2.EPD_WIDTH, epd4in2.EPD_HEIGHT), 255)
 	draw = ImageDraw.Draw(image)
 	draw.text((10, 100), "Noe gikk galt :(", font = font_biggest, fill = 0)
 	draw.text((10, 170), err_msg, font = font_med, fill = 0)
-		
+
 	# Draw error message
 	epd.display(epd.getbuffer(image))
 	time.sleep(2)
 	epd.sleep()
-		
+
 	exit()
-		
+
+def get_host_ip():
+	try:
+        host_name = socket.gethostname()
+        return socket.gethostbyname(host_name)
+    except:
+        return "Ukjent")
+
 print_weather()
